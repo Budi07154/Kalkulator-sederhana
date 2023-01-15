74042363f34d9d284df42b5ffea1c424ec5de5fb
Budi07154 <74415852+Budi07154@users.noreply.github.com>
2023-01-15 12:31:24 +0700

    Add files via upload

---
+++ modul 4 asistensi header.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+int tambah (float a, float b){
+	return ( a + b );
+	}
+
+int kurang (float a, float b ){
+	return ( a - b );
+	}
+
+int kali (float a, float b ){
+	return ( a * b );
+	}
+
+int bagi (float a, float b ){
+	return (a / b );
+	}
+
+int pangkat (float a, float b ){
+	return pow(a , b );
+	}
---
+++ modul 4 asistensi.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include "modul 4 asistensi header.h"
+using namespace std;
+
+int main (){
+int bil1, bil2;
+char pilihan;
+
+cout<<" ========================================"<<endl;
+cout<<" |	Program Kalkulator Sederhana	|"<<endl;
+cout<<" ========================================"<<endl;
+cout<<endl;
+cout<<" ========================================"<<endl;
+cout<<" |	     Masukan Pilihan		|"<<endl;
+cout<<" ========================================"<<endl;
+cout<<" |					|"<<endl;
+cout<<" |	(A) Tambah	(B) Kurang (E) Pangkat	|"<<endl;
+cout<<" |	(C) Kali	(D) Bagi	|"<<endl;
+cout<<" |					|"<<endl;
+cout<<" ========================================"<<endl;
+cout<<" Masukan Pilihan		: ";cin>>pilihan;
+cout<<endl;
+
+if ( pilihan == 'A' || pilihan == 'a' ) {
+
+	cout<<" ========================================"<<endl;
+	cout<<" |	     Masukan Bilangan		|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" |					|"<<endl;
+	cout<<" | Masukan Bilangan pertama : ";cin>>bil1;
+	cout<<" | Masukan Bilangan kedua : ";cin>>bil2;
+	cout<<" |					|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" | Hasilnya Adalah : "<<tambah(bil1,bil2)<<"			|"<<endl;
+	cout<<" ========================================"<<endl;
+
+}
+else if ( pilihan == 'B' || pilihan == 'b' ) {
+
+	cout<<" ========================================"<<endl;
+	cout<<" |	     Masukan Bilangan		|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" |					|"<<endl;
+	cout<<" | Masukan Bilangan pertama : ";cin>>bil1;
+	cout<<" | Masukan Bilangan kedua : ";cin>>bil2;
+	cout<<" |					|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" | Hasilnya Adalah : "<<kurang(bil1,bil2)<<"			|"<<endl;
+	cout<<" ========================================"<<endl;
+
+}
+else if ( pilihan == 'c' || pilihan == 'C' ) {
+
+	cout<<" ========================================"<<endl;
+	cout<<" |	     Masukan Bilangan		|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" |					|"<<endl;
+	cout<<" | Masukan Bilangan pertama : ";cin>>bil1;
+	cout<<" | Masukan Bilangan kedua : ";cin>>bil2;
+	cout<<" |					|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" | Hasilnya Adalah : "<<kali(bil1,bil2)<<"			|"<<endl;
+	cout<<" ========================================"<<endl;
+}
+else if ( pilihan == 'D' || pilihan == 'd' ) {
+
+	cout<<" ========================================"<<endl;
+	cout<<" |	     Masukan Bilangan		|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" |					|"<<endl;
+	cout<<" | Masukan Bilangan pertama : ";cin>>bil1;
+	cout<<" | Masukan Bilangan kedua : ";cin>>bil2;
+	cout<<" |					|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" | Hasilnya Adalah : "<<bagi(bil1,bil2)<<"			|"<<endl;
+	cout<<" ========================================"<<endl;
+}
+else if ( pilihan == 'E' || pilihan == 'e' ) {
+
+	cout<<" ========================================"<<endl;
+	cout<<" |	     Masukan Bilangan		|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" |					|"<<endl;
+	cout<<" | Masukan Bilangan pertama : ";cin>>bil1;
+	cout<<" | Masukan Bilangan kedua : ";cin>>bil2;
+	cout<<" |					|"<<endl;
+	cout<<" ========================================"<<endl;
+	cout<<" | Hasilnya Adalah : "<<pangkat(bil1,bil2)<<"			|"<<endl;
+	cout<<" ========================================"<<endl;
+
+	cout<<endl;
+	cout<<" ";
+}
+}
